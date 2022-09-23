e70cd914a561abc94d94d1c85baf5d2da7457063
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-09-22 21:18:45 -0300

    contando letras e palavras

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
-const numeroFrase = $(".frase").text().split(' ').length;
+const numeroFrase = $(".frase").text().split(/\S+/).length;
 
 $("#tamanho-frase").text(numeroFrase);
+
+const campo = $(".campo-digitacao");
+
+campo.on("input", () => {
+  const numerosCaracteres = campo.val().length;
+
+  $("#contador-caracteres").text(numerosCaracteres);
+  $("#contados-palavras").text(campo.val().split(/\S+/).length - 1)
+});
