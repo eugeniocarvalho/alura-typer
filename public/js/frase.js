2aeccb034011a074d798988d2c1f8f6f5cde7abc
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-10-03 10:44:50 -0300

    solicitando dados com AJAX

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,13 +1,57 @@
 $("#botao-frase").click(geraFraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
+
 
 function geraFraseAleatoria() {
-  $.get("http://localhost:3000/frases", trocaFrase);
+  $("#spinner").fadeIn();
+  $.get("http://localhost:3000/frases", trocaFraseAleatorio)
+    .fail(function () {
+      $("#erro").fadeIn();
+
+      setTimeout(() => {
+        $("#erro").fadeOut();
+      }, 2000);
+
+    })
+    .always(() => {
+      $("#spinner").fadeOut();
+    });
 }
 
-function trocaFrase(data) {
+function trocaFraseAleatorio(data) {
   const randomNumber = Math.floor(Math.random() * data.length);
   const frase = data[randomNumber].texto;
   $(".frase").text(frase);
   atualizaTamanhoFrase();
   atualizaTempoInicial(data[randomNumber].tempo);
+}
+
+function buscaFrase() {
+  const fraseId = $("#frase-id").val();
+  const dados = {
+    id: fraseId
+  };
+
+  $("#spinner").fadeIn();
+
+  $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function () {
+      $("#erro").fadeIn();
+
+      setTimeout(() => {
+        $("#erro").fadeOut();
+      }, 2000);
+
+    })
+    .always(() => {
+      $("#spinner").fadeOut();
+    });
+}
+
+function trocaFrase(data) {
+  const frase = $(".frase");
+
+  frase.text(data.texto);
+  atualizaTamanhoFrase();
+  atualizaTempoInicial(data.tempo);
 }
